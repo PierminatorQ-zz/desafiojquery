c0604d885c99663cc9f1864d780bbf0aaa0191c1
PierminatorQ <pierrequintero@gmail.com>
2020-10-05 20:14:33 -0300

    first commit

---
+++ app/assets/javascripts/alert.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  
+  
+  $('#alert').click(function() {
+      $('#posalert').fadeIn('fast');      
+  })
+
+  $('#close').click(function() {
+      $('#posalert').fadeOut("slow");
+  })
+})
+
---
+++ app/assets/javascripts/collapse.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+
+
+$('#toggle').click(function (){
+
+  if($('#caja').css('display') != 'none'){
+    $('#caja').slideUp('fast');
+  } else {
+    $('#caja').slideDown('fast');
+  }
+
+})
+
+  
+
+})
+
